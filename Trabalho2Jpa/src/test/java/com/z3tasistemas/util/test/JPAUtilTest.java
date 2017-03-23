97b16eb6c15cc231e0c1a2144f73368bac88514d
MGO108-TI-14937\ti02 <leandroz3ta@gmail.com>
2017-03-23 17:58:21 -0300

    Alteração de pacote

---
+++ Trabalho2Jpa/src/test/java/com/z3tasistemas/util/test/JPAUtilTest.java
@@ -0,0 +1,31 @@
+package com.z3tasistemas.util.test;
+
+import org.junit.Test;
+
+import com.z3tasistemas.base.test.Basetest;
+
+import static org.junit.Assert.*;
+
+public class JPAUtilTest extends Basetest{
+
+	@Test
+	public void deveTerInstanciaDoEntityManagerDefinida() {
+		assertNotNull("instância do EntityManager não deve estar nula", em);
+	}
+	
+	@Test
+	public void deveFecharEntityManager() {
+		em.close();
+		
+		assertFalse("instância do EntityManager deve estar fechada", em.isOpen());
+	}
+	
+	@Test
+	public void deveAbrirUmaTransacao() {
+		assertFalse("transação deve estar fechada", em.getTransaction().isActive());
+		
+		em.getTransaction().begin();
+		
+		assertTrue("transação deve estar aberta", em.getTransaction().isActive());
+	}
+}
